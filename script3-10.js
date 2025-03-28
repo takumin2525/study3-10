335b99882048ac185ab227595d36ab39596915ff
takumin2525 <324works@gmail.com>
2025-03-28 13:01:59 +0900

    Js11完了

--- script3-10.js
+++ script3-10.js
@@ -1,23 +1,29 @@
 window.addEventListener('load', function(){
     const pullDownButton = document.getElementById("lists");
-    console.log(pullDownButton);
+    const pullDownParets = document.getElementById("pull-down");
 })
 
 lists.addEventListener('mouseover', function(){
     const pullDownButton = document.getElementById("lists");
-    pullDownButton.setAttribute("style", "background-color:blue;")
+    this.setAttribute("style", "background-color:blue;")
     console.log("乗った時は青色");
 })
 
 lists.addEventListener('click', function(){
     const pullDownButton = document.getElementById("lists");
-    pullDownButton.setAttribute("style", "background-color:green;")
-    console.log("クリック時は緑色");
+    const pullDownParets = document.getElementById("pull-down");
+    if(pullDownParets.getAttribute("style") == "display:block;"){
+        pullDownParets.removeAttribute("style", "display:block;")
+        console.log("非表示")
+    } else{
+        pullDownParets.setAttribute("style", "display:block;")
+        console.log("表示")
+    }
 })
 
 lists.addEventListener('mouseout', function(){
     const pullDownButton = document.getElementById("lists");
-    pullDownButton.removeAttribute("style", "background-color:blue;"
+    this.removeAttribute("style", "background-color:blue;"
     )
     console.log("外れた時は赤色");
 })
